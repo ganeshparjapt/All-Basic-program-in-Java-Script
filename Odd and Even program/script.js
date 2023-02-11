183b1b63447420c2b55c9e7368d98927df429f07
ganesh <ganeshparjaptm5@gmail.com>
2023-02-11 17:12:31 +0530

    modificatio script.js

--- Odd and Even program/script.js
+++ Odd and Even program/script.js
@@ -1,1 +1,12 @@
-console.log(" i am console scren")
+const number = prompt("Enter the number to see odd and even")
+//logic
+if(number< 1){
+    console.log("plase Enter positiv number")
+}
+else if(number%2 === 0){
+    console.log(`${number} is Even number ` )
+}
+else{
+    console.log(`${number} is odd number ` )
+
+}
