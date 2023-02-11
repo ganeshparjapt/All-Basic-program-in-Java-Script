d7bb3bae97f3c5e11c34a5885ffc85e985b2199e
ganesh <ganeshparjaptm5@gmail.com>
2023-02-12 01:08:37 +0530

    return by function

---
+++ Sum of Two number using function/script.js
@@ -0,0 +1,7 @@
+function add(a,b) {
+    return a+b;
+}
+const number1 = parseInt( prompt("Enter the number first : "));
+const number2 = parseInt( prompt("Enter the number second : "));
+let result = add(number1,number2);
+console.log(result)
