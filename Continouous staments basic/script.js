fdbe3b74f4ab0c7fa78828fe08c940863bd79bb0
ganesh <ganeshparjaptm5@gmail.com>
2023-02-11 23:45:03 +0530

    continous staments using

---
+++ Continouous staments basic/script.js
@@ -0,0 +1,10 @@
+const number = prompt("enter a number to print seress: ");
+let a,b;
+//logic
+console.log("your number series is this :")
+for(let i=1;i<= number;i++){
+    if(i == 3){
+        continue
+    }
+    console.log(i)
+}
