a7193fdae84db87ba2c52a0068efb5e6a3162c02
ganesh <ganeshparjaptm5@gmail.com>
2023-02-11 20:41:28 +0530

    Add while loop program

---
+++ While Loop programs/Display number/script.js
@@ -0,0 +1,9 @@
+const number = prompt("Enter the number to print math up to it : ");
+//logic using while loop
+let i=1
+console.log("your number is up to")
+while( i <= number){
+    console.log(i);
+    i++
+}
+
