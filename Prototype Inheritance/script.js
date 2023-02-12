fed9a86cb1430cf48de79e3c12aac55070aad1bf
ganesh <ganeshparjaptm5@gmail.com>
2023-02-12 23:45:34 +0530

    modification

--- Prototype Inheritance/script.js
+++ Prototype Inheritance/script.js
@@ -1,15 +1,17 @@
-//constructor function fu\
+//cuntructor function 
 function student(){
-    this.name ='ganesh prajapt',
-    this.age = 23
+    this.name= 'ganesh prajapt',
+    this. age = 23
 }
 //create object
-const person1 = new student();
-const person2 = new student();
-//adding property to constructor function
-student.prototype.gender='male';
-//prototype value of student
-console.log(student.prototype);
-//inheriting the property form prototype
-console.log(person1.gender);
-console.log(person2.age);
+const student1 = new student();
+const student2 = new student();
+//adding property to constructer function
+student.prototype.gender ='male';
+student.prototype.Fname = 'kishan ram prajtp'
+//prototype value of the student 
+console.log(student.prototype)
+//inherit the property of prototype
+console.log(student1.gender)
+console.log(student1.age)
+console.log(student1.Fname)
