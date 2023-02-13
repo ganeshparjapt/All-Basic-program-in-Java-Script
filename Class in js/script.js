4f3050c06a7fb097ec9d5766d832cace94d4bab1
ganesh <ganeshparjaptm5@gmail.com>
2023-02-13 19:07:30 +0530

    Class in java script

---
+++ Class in js/script.js
@@ -0,0 +1,11 @@
+class person{
+    constructor(name){
+        this.name =name;
+    }
+}
+//creatin an objects
+const person1 = new person('ganesh');
+const person2 = new person('sandeep');
+
+console.log(person1.name);
+console.log(person2.name);
