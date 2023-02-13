0756d668ba5009e1bc01f57754fd51a70cdd9bc2
ganesh <ganeshparjaptm5@gmail.com>
2023-02-13 14:08:51 +0530

    Number in java script

---
+++ Number in java script/script.js
@@ -0,0 +1,19 @@
+const a = 45;
+//creation a number object
+const b = new Number(45);
+
+console.log(a);
+console.log(b);
+
+console.log(typeof a); 
+console.log(typeof b); 
+///check the number of types
+const c =12;
+console.log(Number.isInteger (c));
+console.log(Number.isInteger (c));
+///check if c is NaN
+const d= NaN;
+console.log(Number.isNaN(d));
+//display upto thwo decimal point
+const e =5.1452;
+console.log(d.toFixed(2));
