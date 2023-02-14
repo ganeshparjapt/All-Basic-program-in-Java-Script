92940e1e317dd9c9f12663cd5f3283aad9e378e8
ganesh <ganeshparjaptm5@gmail.com>
2023-02-15 01:01:11 +0530

    add files Fabboncis seres

---
+++ Fabonacci series-2/script.js
@@ -0,0 +1,13 @@
+//print fabonccis seres
+const number = parseInt(prompt("Enter the number to print Fabonacci series : "));
+//logic 
+let n1= 0,n2=1,tream;
+console.log(n1);
+for(let i =0; i < number ;i ++){
+    
+    console.log(tream);
+    tream = n1 + n2;
+    n1= n2;
+    n2 = tream;
+
+}
