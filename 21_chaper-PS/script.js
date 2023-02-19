8359c8d8ceb6c5460a3d437909919713c3aadcaa
ganesh <ganeshparjaptm5@gmail.com>
2023-02-19 13:43:11 +0530

    prectis set on reduce map ,filter

---
+++ 21_chaper-PS/script.js
@@ -0,0 +1,33 @@
+let arr = [34,45,56,45,3,42,30,30,3];
+console.log(arr);
+let newarrr = parseInt(prompt("Enter the value of N to add number in array"));
+arr.push(newarrr);
+console.log(arr);
+//prectice problem -2
+let a;
+do{
+    a = parseInt(prompt("Enter the nubmwer"));
+    
+    arr.push(a);
+    }
+    while(a != 0){
+        console.log(arr);
+    }
+let  fi = (x)=>{
+    return x % 10 === 0;
+}
+let newarr = arr.filter((x)=>{
+    return x%10 === 0;
+});
+// console.log(newarr);
+let arrforReduce =[1,2,3,8]
+let fii = (h1,h2) =>{
+    return h1*h2
+}
+let aa= arrforReduce.pop();
+console.log(aa);
+let newarr2 = arr.map((x)=>{
+    return x*x;
+})
+console.log(newarr2);
+console.log(arrforReduce.reduce(fii));
