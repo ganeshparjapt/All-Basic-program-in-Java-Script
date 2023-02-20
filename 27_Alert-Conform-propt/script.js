43d6fc0bb4b96d2635c9becba388a5bdfc671f32
ganesh <ganeshparjaptm5@gmail.com>
2023-02-20 23:10:17 +0530

    Miner change now

--- 27_Alert-Conform-propt/script.js
+++ 27_Alert-Conform-propt/script.js
@@ -6,6 +6,7 @@ alert("you endterd a  of type  "+ (typeof a));
 let write = confirm("do you want ot write it to the page")
 if(write){
     document.write(a)
+    document.write(a)
 }
 else{
     document.write("please alow me to write")
