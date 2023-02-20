d1c1ea59e1c2a21c795fbc57a8d5b5e6845e378c
ganesh <ganeshparjaptm5@gmail.com>
2023-02-20 23:35:56 +0530

    body releted

---
+++ 28-Dom and Bom/script.js
@@ -0,0 +1,7 @@
+//DOM /document object model
+console.log(window)
+console.log(document.body)//direct body ko dekne ke lye use hota  he change bhi kar skte he     
+document.body.style.backgroundColor="black";
+document.body.style.color="white";
+
+//BOM /boruser object model
