a3e92fb264c8c65f60be6600e8a57dde6f5db4e4
ganesh <ganeshparjaptm5@gmail.com>
2023-03-12 22:34:11 +0530

    add simpe object model

---
+++ 32-children/index.js
@@ -0,0 +1,7 @@
+console.log(document.body)
+console.log(document.body.firstChild)
+console.log(document.body.lastChild)
+console.log(document.body.childNodes)
+let arr = Array.from(document.body.childNodes);
+console.log(arr)
+
