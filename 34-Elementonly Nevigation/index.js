3b814b32f12849ff23cc6913e6a93385b0dc2224
ganesh <ganeshparjaptm5@gmail.com>
2023-03-16 19:41:19 +0530

    add element nevigation

---
+++ 34-Elementonly Nevigation/index.js
@@ -0,0 +1,11 @@
+const changebgred =()=>{
+    document.body.firstElementChild.style.background = "#317431"
+    document.body.firstElementChild.style.color = "white"
+    
+}
+let b = document.body
+
+console.log("fist child of b is :", b.firstChild);
+console.log("fist child of b is :", b.firstElementChild);
+console.log(document.nextSibling)
+console.log(b.nextSibling)
