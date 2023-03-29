ccf0d40f86d2892a6b6601a4ff6d538f9cc35524
ganesh <ganeshparjaptm5@gmail.com>
2023-03-29 22:15:08 +0530

    changess

--- 31-DOM/index.js
+++ 31-DOM/index.js
@@ -1,1 +1,11 @@
 console.log("i am dom");
+let n=[3,4,5,6,7,8,9,]
+console.log(n);
+let newn = n.slice(1,3);
+console.log(newn);
+let number =[1,2,3,4,5]
+console.log(number)
+let numbersplice =number.splice(2,2,23,23);
+console.log(numbersplice)
+console.log(number)
+
