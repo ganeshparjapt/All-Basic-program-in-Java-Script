584180875e1534c2c52c69aec8cc8a0c75232c15
ganesh <ganeshparjaptm5@gmail.com>
2023-03-31 22:55:10 +0530

    modify

---
+++ prime or not prime/index.js
@@ -0,0 +1,20 @@
+let number = parseInt(prompt("Enter the value : "));
+let prime
+if(number === 1 ){
+    prime = false
+}
+else{
+    for(let i=2;i<= number;i++){
+        prime = true;
+        break;
+    }
+
+
+}
+if(prime = true){
+    console.log("your number is prime : "+number);
+}
+if(prime = false){
+    console.log("you num ber is not prime : "+number)
+    console.log("you num ber is not prime : "+number)
+}
