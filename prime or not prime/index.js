869851a6453b080cbb9c9deecd8c4591da8faac7
ganesh <ganeshparjaptm5@gmail.com>
2023-04-01 09:18:41 +0530

    modify

--- prime or not prime/index.js
+++ prime or not prime/index.js
@@ -1,27 +1,31 @@
-let number = parseInt(prompt("Enter the value : "));
-let prime
-if(number === 1 ){
-    prime = true
-}
-else{
-    for(let i=2;i<= number;i++){
-        if(number % i === 0 ){
-            prime = true;
-            break;
-        }
-        else{
-            prime = false
-        }
-        
-       
+function isPrime(num) {
+    // convert the string to a number using parseInt
+    num = parseInt(num);
+  
+    // if the number is less than 2, it's not prime
+    if (num < 2) {
+      return false;
     }
+  
+    // loop through all the numbers from 2 to the square root of the number
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      // if the number is divisible by any number between 2 and its square root, it's not prime
+      if (num % i === 0) {
+        return false;
+      }
+    }
+  
+    // if the number is not divisible by any number between 2 and its square root, it's prime
+    return true;
+  }
 
+  const input = prompt("Enter a number: ");
+const num = parseInt(input);
 
+if (isPrime(num)) {
+  console.log(num + " is prime");
+} else {
+  console.log(num + " is not prime");
 }
-if(prime = true){
-    console.log("your number is prime : "+number);
-}
-if(prime = false){
 
-    console.log("you num ber is not prime : "+number)
-}
+  
